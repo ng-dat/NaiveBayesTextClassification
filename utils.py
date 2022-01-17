1314de2518860fce75475473f2b3548aac3b4409
GattoMac <traceformula2000@gmail.com>
2022-01-16 21:52:34 -0800

    Initialize project

---
+++ utils.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+def extract_vocabulary():
+    pass
+
+
+def get_train_paths():
+    pass
+
+
+def get_test_paths():
+    pass
+
+
+def text_preprocess():
+    pass
+
+
+def tokenize():
+    pass
+
+
+def read_sentence_from_file():
+    pass
+
+
+def read_model_from_file():
+    pass
+
+
+def write_model_to_file():
+    pass
+
+
+def write_prediction_to_file():
+    pass
+
+
+def read_prediction_from_file():
+    pass
+
+
+def score_predictions_f1():
+    pass
+
+
+def score_predictions_f1_truthful():
+    pass
+
+
+def score_predictions_f1_deceptive():
+    pass
+
+
+def score_predictions_f1_positive():
+    pass
+
+
+def score_predictions_f1_negative():
+    pass
